2d1d4965d92fc0937d9053663c373cf07fedc297
Alban <alban.grison@gmail.com>
2015-05-25 08:16:18 +0200

    initial commit

---
+++ lib/guppy_store_memory.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The guppy_store_memory library.
+///
+/// This is an awesome library. More dartdocs go here.
+library guppy_store_memory;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/guppy_store_memory_base.dart';
